f8a8c5761acc2703ac87451673a9df4e08dbf26c
liwentian <wentian.li@hobot.cc>
2017-07-28 21:24:32 +0800

    fd

--- preview2/app/src/main/java/com/inuker/preview2/MainActivity.java
+++ preview2/app/src/main/java/com/inuker/preview2/MainActivity.java
@@ -1,12 +1,55 @@
 package com.inuker.preview2;
 
 import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.TextureView;
 
-public class MainActivity extends Activity {
+import java.io.IOException;
+
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+
+    private Camera mCamera;
+
+    private TextureView mTextureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mTextureView = new TextureView(this);
+        setContentView(mTextureView);
+
+        mTextureView.setSurfaceTextureListener(this);
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open(1);
+
+        try {
+            mCamera.setPreviewTexture(mTextureView.getSurfaceTexture());
+            mCamera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
     }
 }
