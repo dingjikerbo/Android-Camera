aab644e7b8bc35b468887711f9c787c6c490b026
liwentian <wentian.li@hobot.cc>
2017-07-29 15:48:48 +0800

    fd

---
+++ recorder2/app/src/main/java/com/inuker/recorder2/MainActivity.java
@@ -0,0 +1,77 @@
+package com.inuker.recorder2;
+
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+
+    private TextureView mTextureView;
+
+    private CameraSurfaceRender mRenderer;
+
+    private GLThread mGLThread;
+
+    private Button mButton;
+
+    private boolean isRecording;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextureView = (TextureView) findViewById(R.id.texture);
+        mTextureView.setSurfaceTextureListener(this);
+
+        mButton = (Button) findViewById(R.id.btn);
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                isRecording = !isRecording;
+
+                if (isRecording) {
+                    mButton.setText("stop record");
+                } else {
+                    mButton.setText("start record");
+                }
+                mRenderer.toggleRecord(v);
+            }
+        });
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.v("bush", "onSurfaceTextureAvailable");
+        mRenderer = new CameraSurfaceRender(mTextureView);
+        mGLThread = new GLThread(surface, mRenderer);
+        mGLThread.start();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+}
