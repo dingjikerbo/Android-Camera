81a61fe212b61103d7ba6682550d6f4e7669d7fe
liwentian <wentian.li@hobot.cc>
2017-07-29 08:12:53 +0800

    fd

---
+++ recorder1/app/src/main/java/com/inuker/recorder1/Constants.java
@@ -0,0 +1,12 @@
+package com.inuker.recorder1;
+
+/**
+ * Created by liwentian on 17/7/29.
+ */
+
+public class Constants {
+
+    public static final int WIDTH = 1920;
+
+    public static final int HEIGHT = 1080;
+}
